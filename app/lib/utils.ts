9cdad5f8dc56a95144ab6c5ac9af8c7183efbfe7
TanikshaSingh20 <tanikshasingh20@gmail.com>
2026-05-28 22:58:50 +0530

    Implement wipe functionality

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,21 +1,18 @@
-// app/lib/utils.ts
-import type {ClassValue} from "clsx";
-export function cn(... inputs:ClassValue[]) {
-    return twMerge(clsx(... inputs))
-}
-export function formatSize(bytes: number): string {
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(...inputs));
+}
+
+export function formatSize(bytes: number): string {
     if (bytes === 0) return '0 Bytes';
 
     const k = 1024;
-
     const sizes: string[] = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-
     const i: number = Math.floor(Math.log(bytes) / Math.log(k));
 
     return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
-
 }
 
-export const generateUUID=() => crypto.randomUUID();
-
+export const generateUUID = () => crypto.randomUUID();
